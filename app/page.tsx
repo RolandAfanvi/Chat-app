9edc71be281a25cd4be00dc9ea548740148fdfd2
RolandAfanvi <125564906+RolandAfanvi@users.noreply.github.com>
2024-06-14 12:51:37 +0200

    Update UI by adjusting some features

--- app/page.tsx
+++ app/page.tsx
@@ -11,20 +11,30 @@ export default function Home() {
   return (
     <section className="w-full h-screen flex items-center justify-center p-3">
       <div className="max-w-[1000px] m-auto flex items-center justify-center flex-col gap-5 text-center">
-        <h1 className="text 6xl uppercase font-black">CHAT
+        <h1 className="text-6xl uppercase font-black">CHAT
           <span className="text-blue-600">APP</span>
         </h1>
-        <p>
-        Encryption is the process of encoding information. This process converts the original representation 
-        of the information, known as plaintext, into an alternative form known as ciphertext. Ideally, only a
-        uthorized parties can decipher a ciphertext back to plaintext and access the original information. 
-        Encryption does not itself prevent interference but denies the intelligible content to a would-be 
-        interceptor. Encryption is a two-way function; what is encrypted can be decrypted with the proper key.
-        </p>
+
+        <div className="container mx-auto px-4 py-8">
+        
+        <div className="bg-white  text-black rounded-lg p-6 mt-4">
+         
+
+          <h2 className="text-xl uppercase font-semibold mb-2">Pourquoi Choisir ChatApp?</h2>
+          <p>ChatApp est plus qu'une simple application de chat ; c'est une plateforme qui valorise les interactions humaines dans un monde digital. Notre mission est de fournir un service de chat sécurisé, convivial et accessible à tous. Que vous cherchiez à rester en contact avec vos proches, à collaborer avec des collègues ou simplement à discuter de vos passions préférées, ChatApp est là pour vous accompagner.</p>
+          <p className="mt-4"><strong>Rejoignez la communauté ChatApp aujourd'hui</strong> et découvrez pourquoi tant d'utilisateurs choisissent cette application pour leurs besoins de communication instantanée.</p>
+        
+        </div>
+
+ 
+      </div>
+
+     
         <button onClick={loginWithGoogle} className="bg-gray-200 hover:bg-gray-300 text-gray-900 rounded-md p-2 flex items-center gap-2">
           <FcGoogle />
           <span> Se connecter avec Google</span>
         </button>
+        
       </div>
     </section>
 
--- db/configFirebase.js
+++ db/configFirebase.js
@@ -14,7 +14,6 @@ const firebaseConfig = {
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
-
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app)
 export const auth = getAuth(app)
